02ffcd4833ddca515af44cd64f4d80bab73766f4
Jonathan D. Sterk <jdsterk@sandia.gov>
2025-03-17 10:46:52 -0600

    Forgot a few imports

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     packages=find_packages(where='src'),
     # package_data={  'ionsim.physics'  :['data/*.data'],
     #                 'ionsim.tests'    :['functionTestData/*']},
-    install_requires=['numpy','scipy','matplotlib', "csaps"],
+    install_requires=['numpy','scipy','matplotlib', "csaps", "icecream", "pyyaml", "nptyping"],
     platforms=['any'],
     classifiers=filter(None, classifiers.split("\n")),
 )
