5362aa56bf8ddffb496e128dc3419ad2b9b0faf0
Jay Van Der Wall <jayvand@sandia.gov>
2025-06-06 17:02:03 -0600

    Use TypeAlias if Python >= 3.10

--- src/ionsim/custom_types.py
+++ src/ionsim/custom_types.py
@@ -1,12 +1,23 @@
-from typing import Any
+import sys
+from typing import Any, TypeAlias
 from numpy.typing import NDArray
 from scipy.sparse import spmatrix
 
-Vector = NDArray[Any]
-Matrix = NDArray[Any]
+if sys.version_info >= (3, 10):
+    Vector: TypeAlias = NDArray[Any]
+    Matrix: TypeAlias = NDArray[Any]
 
-SparseVector = spmatrix
-SparseMatrix = spmatrix
+    SparseVector: TypeAlias = spmatrix
+    SparseMatrix: TypeAlias = spmatrix
 
-AnyVector = Vector | SparseVector
-AnyMatrix = Matrix | SparseMatrix
+    AnyVector: TypeAlias = Vector | SparseVector
+    AnyMatrix: TypeAlias = Matrix | SparseMatrix
+else:
+    Vector = NDArray[Any]
+    Matrix = NDArray[Any]
+
+    SparseVector = spmatrix
+    SparseMatrix = spmatrix
+
+    AnyVector = Vector | SparseVector
+    AnyMatrix = Matrix | SparseMatrix
