207c4ce0a96ff96cb64d15a07b8be759e4aa55b8
Ethan McGarrigle <ecmcgar@sandia.gov>
2026-03-16 17:04:02 -0600

    add general operator t-evolution in operator testing

--- tests/test_operator.py
+++ tests/test_operator.py
@@ -6,6 +6,8 @@ from ionsim.energy_level import EnergyEigenstate
 from ionsim.basis import StandardBasis
 from ionsim.operator import CouplingOperator, EnergyShiftOperator, GeneralOperator
 from ionsim.testing import assert_array_close
+from ionsim.state import State
+from ionsim.hamiltonian import Hamiltonian 
 
 class TestOperator(unittest.TestCase):
 
@@ -44,6 +46,25 @@ class TestOperator(unittest.TestCase):
 
         assert_array_close(energy_shift_contribution.static_matrix.toarray(), np.diag(np.diag(big_general_matrix)))
         assert_array_close(coupling_op_contribution.static_matrix.toarray(), big_general_matrix - np.diag(np.diag(big_general_matrix)) )
+
+    def test_time_evolution(self):
+        """ Test Hamiltonian creation and time evolution of a general operator """ 
+        state_coeffs = [1., 0., 0., 0.]
+        psi_init = State.from_coefficients(self.basis, state_coeffs)
+        big_general_matrix = np.kron(self.static_matrix_input, np.eye(2))
+
+        # Create the Hamiltonian from the general operator 
+        test_operator = GeneralOperator.from_matrix(self.basis, big_general_matrix)
+        frame_energies = [-state.energy for state in self.basis.states]
+        test_hamiltonian = Hamiltonian(self.basis, [self.general_op], frame_energies, sparse = False) 
+
+        # Test time propagation of the initial state: 
+        duration = 1E-6
+        psi_t = psi_init.propagate_using_schrodinger_equation(test_hamiltonian, duration)
+
+        probabilities = psi_t.compute_basis_state_probabilities()
+        expected_probabilities = np.array([1., 0., 0., 0.])
+        assert_array_close(probabilities, expected_probabilities)
 
 
 if __name__ == '__main__':
