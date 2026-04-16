150d8faac76346e729310077925609e896c42131
Ethan McGarrigle <ecmcgar@sandia.gov>
2026-04-16 10:37:18 -0600

    finalize branch for MR

--- tests/test_state.py
+++ tests/test_state.py
@@ -39,20 +39,17 @@ class TestState(unittest.TestCase):
         domain_limit = 2.
         x_grid = np.linspace(-domain_limit, domain_limit, 25)
         p_grid = np.linspace(-domain_limit, domain_limit, 25)
-        
         W_distribution = self.spin_motion_state.compute_wigner_distribution(x_grid, p_grid)[0]        
 
-        W_max = np.max(W_distribution)
         # Test just one slice of the array and half of its contents (U(1) symmetric about origin)) 
         slice_indx = 12
         W_expected_at_slice = np.array([0.00583005, 0.0110443, 0.0197914, 0.03354962, 0.05379861, 0.08160694, 0.11709966, 0.15894861, 0.20409406, 0.24789999, 0.2848362, 0.30958962, 0.31830989]) 
-
         norm = np.trapezoid(np.trapezoid(W_distribution, x_grid, axis=0), p_grid)
         expected_norm = 0.9902872798196112
         self.assertAlmostEqual(norm, expected_norm, places=7)
         expected_W_max = 0.31830988618379075
+        W_max = np.max(W_distribution)
         self.assertAlmostEqual(W_max, expected_W_max, places=7)
-
         assert_array_close(W_distribution[slice_indx, 0:13], W_expected_at_slice, rtol=1e-04, atol=1E-7) 
 
 
