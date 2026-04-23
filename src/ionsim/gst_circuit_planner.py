9314a0bb0ebe13a4f1a3d3cfe64c750bba2799a9
Ethan McGarrigle <ecmcgar@sandia.gov>
2026-04-23 17:24:09 -0600

    add linear vs long-form gst circuit planning, write circuit to file

--- src/ionsim/gst_circuit_planner.py
+++ src/ionsim/gst_circuit_planner.py
@@ -38,8 +38,83 @@ class GSTCircuitPlanner:
             self.gate_lookup[name] = ParsedGate(name, tuple(qubit_labels))
 
 
-    def generate_circuits(eslf):
-        """ Build the GST circuits 
+    def generate_gst_circuits(self):
+        """ Generate the GST circuits to be ran in experiments. Avoid duplicates """ 
+        gst_circuits = []
+        unique = set() 
+
+        # Combine circuits from linear GST (no germ power) with long-form GST:     
+        for circ in (self._linear_gst_circuits() + self._long_gst_circuits()):
+            raw_circuit_name = circ.build_circuit_string()
+            if raw_circuit_name not in unique:
+                seen.add(raw_circuit_name)
+                circuits.append(circ)
+            
+        self.gst_circuits = circuits
+        return circuits 
+
+
+
+    def _linear_gst_circuits(self):
+        """ Linear GST circuits (no germ powers). Consists of two circuit sets:
+
+            1. Fiducial prep & measure 
+            2. Fidcuial prep, gate, then measure. 
+
+        """ 
+        circuits = []
+
+        # Group 1: Fiducial prep & measure 
+        for prep_fiducial in self.prep_fiducials:
+            for measure_fiducial in self.measure_fiducials:
+                circuits.append( ParsedCircuit.plan(prep_fiducial, germ, power, measure_fiducial, self.qubit_labels)) 
+
+        # Group 2: Fiducial prep, gate, and measure. For each gate, run the prep & measure circuits. 
+        for gate_name in self.gate_names:
+            gate = self.gate_lookup[gate_name] 
+            for prep_fiducial in self.prep_fiducials:
+                for measure_fiducial in self.measure_fiducials:
+                    circuits.append( ParsedCircuit.plan(prep_fiducial, [gate], 1, measure_fiducial, self.qubit_labels)) 
+
+        return circuits 
+
+
+    def _long_gst_circuits(self):
+        """ Long-form GST circuits: fiducial_prep + prep^{germ} + fiducial_measure """ 
+        circuits = []
+        for germ in self.germ:
+            for power in self.germ_powers:
+                for prep_fiducial in self.prep_fiducials:
+                    for measure_fiducial in self.measure_fiducials:
+                        circuits.append( ParsedCircuit.plan(prep_fiducial, germ, power, measure_fiducial, self.qubit_labels)) 
+
+        return circuits 
+
+
+    def write_circuit_plan(self, filepath: str | Path, N_qubits:int = 1):
+        """ Writes a gst data file compatible with the parser """ 
+        if not hasattr(self, 'circuits'):
+            self.generate_gst_circuits() 
+
+        d = 2**N_qubits # Hilbert space dimensionality 
+        outcome_labels = [''.join(bits) for bits in product('01', repeat=N_qubits)] 
+
+        with open(filepath, 'w') as f:
+            # Write the header 
+            columns = ", ".join(f"{outcome} count" for outcome in outcome_labels)
+            f.write(f"## Columns = {columns}\n")
+
+            for circ in self.gst_circuits:
+                f.write(f"{circ.build_circuit_string()}\n")
+                       
+
+    
+
+
+
+
+
+
 
 
  
