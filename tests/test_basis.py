2ddd12b85e3bdde810a2053c22386d351a0fcf46
Ethan McGarrigle <ecmcgar@sandia.gov>
2026-05-26 12:24:34 -0600

    add first test for aliasing

--- tests/test_basis.py
+++ tests/test_basis.py
@@ -9,10 +9,10 @@ class TestBasis(unittest.TestCase):
 
     def setUp(self):
         """Set up the necessary objects for testing."""
-        self.spin_a = AtomicSpin.from_species(species='171Yb+', term_symbols=['S1/2'], level_names=['S1/2,0,0', 'S1/2,1,0'])
-        self.spin_b = AtomicSpin.from_species(species='171Yb+', term_symbols=['S1/2'], level_names=['S1/2,0,0', 'S1/2,1,0'])
-        self.mode_0 = MotionalMode.from_frequency(frequency=3e6*2*np.pi, fock_dimension=3)
-        self.mode_1 = MotionalMode.from_frequency(frequency=4e6*2*np.pi, fock_dimension=2)
+        self.spin_a = AtomicSpin.from_species(species='171Yb+', term_symbols=['S1/2'], level_names=['S1/2,0,0', 'S1/2,1,0'], level_aliases=['0', '1'])
+        self.spin_b = AtomicSpin.from_species(species='171Yb+', term_symbols=['S1/2'], level_names=['S1/2,0,0', 'S1/2,1,0'], level_aliases=['0', '1'])
+        self.mode_0 = MotionalMode.from_frequency(frequency=3e6*2*np.pi, fock_dimension=3, level_aliases = ['Mode 0, n = ' + str(n) for n in range(3)])
+        self.mode_1 = MotionalMode.from_frequency(frequency=4e6*2*np.pi, fock_dimension=2, level_aliases = ['Mode 1, n = ' + str(n) for n in range(2)])
 
     def test_spin_basis_states(self):
         """Test the states of the spin basis."""
@@ -25,6 +25,11 @@ class TestBasis(unittest.TestCase):
         ]
         actual_states = [state.name for state in spin_basis.states]
         self.assertEqual(actual_states, expected_states)
+
+        expected_aliases = ['00', '01', '10', '11']
+        actual_aliases = [state.alias() for state in spin_basis.states]
+
+        self.assertEqual(expected_aliases, actual_aliases)
 
     def test_motional_basis_states(self):
         """Test the states of the motional basis."""
@@ -39,6 +44,14 @@ class TestBasis(unittest.TestCase):
         ]
         actual_states = [state.name for state in motional_basis.states]
         self.assertEqual(actual_states, expected_states)
+
+ #        mode_prefixes = ['Mode 0', 'Mode 1']
+ #        expected_aliases = []
+ #        actual_aliases = [state.alias for ] 
+ #        for mode, prefix in zip(motional_basis.degrees_of_freedom, mode_prefixes):
+ #            expected_aliases.append([prefix + ', n = ' + str(n) for n in range(len(motional_basis.degrees_of_freedom[0].energy_levels))] 
+            #self.assertEqual(actual_states, expected_states)
+
 
     def test_full_basis_states(self):
         """Test the states of the full basis."""
