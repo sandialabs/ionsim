a271ca9f705f35af37975b3e8dd00279c1d12c23
Ethan McGarrigle <ecmcgar@sandia.gov>
2026-07-01 10:21:51 -0600

    update R gate example

--- examples/example_simulated_R_gate.py
+++ examples/example_simulated_R_gate.py
@@ -1,10 +1,3 @@
-# from basis import StandardBasis, XPauliBasis, XPauliAndFockBasis
-# from degree_of_freedom import AtomicStructure, MotionalMode
-# from hamiltonian import Hamiltonian
-# from coupling import CouplingOperator
-# from state import State
-# from named_operators import Pauli, Fock
-
 from pathlib import Path
 
 import ionsim as sm
