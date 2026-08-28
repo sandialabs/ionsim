c1f1321fed0d78dfc94be5d5771ebc31800d6acd
Ethan McGarrigle <ecmcgar@sandia.gov>
2026-08-28 09:25:24 -0600

    rm "if interpolate" in example since its redundant

--- examples/R_gate_interpolation.py
+++ examples/R_gate_interpolation.py
@@ -92,7 +92,6 @@ def main():
     )
     amp_mod = None
 
-    compute_interpolated_gate = True
     test_IO = True
 
     data_directory = Path.home() / "tmp" / "ionsim_examples_data"
@@ -101,105 +100,104 @@ def main():
 
     data_filename = data_directory / "test_R.hdf5"
 
+    ## R Gate interpolation: 
     # Step 1: Set up a grid where you actually build the gates. 
-    if compute_interpolated_gate:
+    phi = 0
+    theta = np.pi/2
 
-        phi = 0
-        theta = np.pi/2
+    # Computing gate on a grid where x is a frequency offset from resonance 
+    #  and y is a noise width. 
+    #  Ex] So y = 0 corresponds to no noise. 
+    #  Ex] x = 0 corresponds to being on resonance with some noise (unless y=0). 
+    dx_name = 'domega'
+    dy_name = 'half_box_width'
 
-        # Computing gate on a grid where x is a frequency offset from resonance 
-        #  and y is a noise width. 
-        #  Ex] So y = 0 corresponds to no noise. 
-        #  Ex] x = 0 corresponds to being on resonance with some noise (unless y=0). 
-        dx_name = 'domega'
-        dy_name = 'half_box_width'
+    if test_IO: 
+        ic(f" -- Reading R gate data from file {data_filename} --- ")
+        # Optional: Write interpolant to a file using gate interpolant class 
+        #R_gate_interpolant.write_to_file(data_filename)
+        R_gate_interpolant_v2 = sm.GateInterpolant.from_file(data_filename, basis)
+        interpolated_R = R_gate_interpolant_v2.interpolated_gate_function
 
-        if test_IO: 
-            ic(f" -- Reading R gate data from file {data_filename} --- ")
-            # Optional: Write interpolant to a file using gate interpolant class 
-            #R_gate_interpolant.write_to_file(data_filename)
-            R_gate_interpolant_v2 = sm.GateInterpolant.from_file(data_filename, basis)
-            interpolated_R = R_gate_interpolant_v2.interpolated_gate_function
-
-            grid_axes = R_gate_interpolant_v2.grid_axes
-            dxs = grid_axes[dx_name]
-            dys = grid_axes[dy_name]
-        else:
-            # Define a gate function to build the gate interpolant. 
-            def R_function(domega, half_box_width):
-                """ Gate function of the interpolation parameters; returns a Gate object """ 
-                # domega and half_box_width are the interpolant parameters 
-                return R(basis, phi, theta, domega, half_box_width, omega, rabi_rate, target_spins, sparse, amp_mod)
+        grid_axes = R_gate_interpolant_v2.grid_axes
+        dxs = grid_axes[dx_name]
+        dys = grid_axes[dy_name]
+    else:
+        # Define a gate function to build the gate interpolant. 
+        def R_function(domega, half_box_width):
+            """ Gate function of the interpolation parameters; returns a Gate object """ 
+            # domega and half_box_width are the interpolant parameters 
+            return R(basis, phi, theta, domega, half_box_width, omega, rabi_rate, target_spins, sparse, amp_mod)
     
-            # 1. Construct the gate interpolant class instance 
-            ic("Building gate interpolant using gate function")
-            gate_name = 'sqrtX'
-            domegas = np.linspace(-50 * 2*np.pi*1e3, 50 * 2*np.pi*1e3, 5) 
-            half_box_widths = np.linspace(0, 50 * 2*np.pi*1e3, 3) 
+        # 1. Construct the gate interpolant class instance 
+        ic("Building gate interpolant using gate function")
+        gate_name = 'sqrtX'
+        domegas = np.linspace(-50 * 2*np.pi*1e3, 50 * 2*np.pi*1e3, 5) 
+        half_box_widths = np.linspace(0, 50 * 2*np.pi*1e3, 3) 
 
-            dxs = domegas
-            dys = half_box_widths
+        dxs = domegas
+        dys = half_box_widths
 
-            grid_axes = {dx_name : dxs, dy_name : dys} 
-            R_gate_interpolant = sm.GateInterpolant.from_gate_function(R_function, grid_axes, gate_name) 
+        grid_axes = {dx_name : dxs, dy_name : dys} 
+        R_gate_interpolant = sm.GateInterpolant.from_gate_function(R_function, grid_axes, gate_name) 
     
-            # 2 Build a gate interpolating function (this uses cubic splines): returns Gate evaluated at grid / off-grid parameter values  
-            """ Ex] interpolated_R(x = 0.5 * 2π * 1E3, y = 2.) returns an R Gate object at domega = 0.5 * 2π * 1E3, half_box_width 2. """ 
-            interpolated_R = R_gate_interpolant.interpolated_gate_function # returns a Gate object at a grid point 
+        # 2 Build a gate interpolating function (this uses cubic splines): returns Gate evaluated at grid / off-grid parameter values  
+        """ Ex] interpolated_R(x = 0.5 * 2π * 1E3, y = 2.) returns an R Gate object at domega = 0.5 * 2π * 1E3, half_box_width 2. """ 
+        interpolated_R = R_gate_interpolant.interpolated_gate_function # returns a Gate object at a grid point 
 
-        dxs2 = np.linspace(dxs[0], dxs[-1], (len(dxs)-1)*2 + 1)
-        dy = dys[-1]
-        ms_gates = []
-        for dx in dxs:
-            ms_gates.append(R(basis, phi, theta, dx, dy, omega, rabi_rate, target_spins, sparse, amp_mod))
-        ms_gates2 = []
-        for dx in dxs2:
-            ms_gates2.append(R(basis, phi, theta, dx, dy, omega, rabi_rate, target_spins, sparse, amp_mod))
-        fidelities = [gate.compute_process_fidelity(ideal_R(basis, phi, theta, target_spins).process_matrix) for gate in ms_gates]
-        fidelities2 = [gate.compute_process_fidelity(ideal_R(basis, phi, theta, target_spins).process_matrix) for gate in ms_gates2]
-        approx_fids = [
-            interpolated_R(dx, dy).compute_process_fidelity(
-                ideal_R(basis, phi, theta, target_spins).process_matrix
-            ) for dx in dxs2
-        ]
+    dxs2 = np.linspace(dxs[0], dxs[-1], (len(dxs)-1)*2 + 1)
+    dy = dys[-1]
+    ms_gates = []
+    for dx in dxs:
+        ms_gates.append(R(basis, phi, theta, dx, dy, omega, rabi_rate, target_spins, sparse, amp_mod))
+    ms_gates2 = []
+    for dx in dxs2:
+        ms_gates2.append(R(basis, phi, theta, dx, dy, omega, rabi_rate, target_spins, sparse, amp_mod))
+    fidelities = [gate.compute_process_fidelity(ideal_R(basis, phi, theta, target_spins).process_matrix) for gate in ms_gates]
+    fidelities2 = [gate.compute_process_fidelity(ideal_R(basis, phi, theta, target_spins).process_matrix) for gate in ms_gates2]
+    approx_fids = [
+        interpolated_R(dx, dy).compute_process_fidelity(
+            ideal_R(basis, phi, theta, target_spins).process_matrix
+        ) for dx in dxs2
+    ]
 
-        plt.rcParams.update({'font.size': 16})
+    plt.rcParams.update({'font.size': 16})
 
-        dx_scale = 1/(2*np.pi*1e3)
-        plt.plot(dxs * dx_scale, 1-np.array(fidelities), 'o', label='simulation: grid point')
-        plt.plot(dxs2 * dx_scale, 1-np.array(approx_fids), '-', label='interpolation')
-        plt.plot(dxs2 * dx_scale, 1-np.array(fidelities2), '.', label='simulation: off grid')
-        plt.xlabel(f'Frequency Error (kHz)')
-        plt.ylabel('Infidelity')
-        plt.legend()
-        plt.savefig(data_directory / f'infidelity_vs_{dx_name}.pdf', bbox_inches='tight')
-        plt.show()
+    dx_scale = 1/(2*np.pi*1e3)
+    plt.plot(dxs * dx_scale, 1-np.array(fidelities), 'o', label='simulation: grid point')
+    plt.plot(dxs2 * dx_scale, 1-np.array(approx_fids), '-', label='interpolation')
+    plt.plot(dxs2 * dx_scale, 1-np.array(fidelities2), '.', label='simulation: off grid')
+    plt.xlabel(f'Frequency Error (kHz)')
+    plt.ylabel('Infidelity')
+    plt.legend()
+    plt.savefig(data_directory / f'infidelity_vs_{dx_name}.pdf', bbox_inches='tight')
+    plt.show()
 
-        dx = dxs[-1]
-        dys2 = np.linspace(dys[0], dys[-1], (len(dys)-1)*2 + 1)
-        ms_gates = []
-        for dy in dys:
-            ms_gates.append(R(basis, phi, theta, dx, dy, omega, rabi_rate, target_spins, sparse, amp_mod))
-        ms_gates2 = []
-        for dy in dys2:
-            ms_gates2.append(R(basis, phi, theta, dx, dy, omega, rabi_rate, target_spins, sparse, amp_mod))
-        fidelities = [gate.compute_process_fidelity(ideal_R(basis, phi, theta, target_spins).process_matrix) for gate in ms_gates]
-        fidelities2 = [gate.compute_process_fidelity(ideal_R(basis, phi, theta, target_spins).process_matrix) for gate in ms_gates2]
-        approx_fids = [
-            interpolated_R(dx, dy).compute_process_fidelity(
-                ideal_R(basis, phi, theta, target_spins).process_matrix
-            ) for dy in dys2
-        ]
+    dx = dxs[-1]
+    dys2 = np.linspace(dys[0], dys[-1], (len(dys)-1)*2 + 1)
+    ms_gates = []
+    for dy in dys:
+        ms_gates.append(R(basis, phi, theta, dx, dy, omega, rabi_rate, target_spins, sparse, amp_mod))
+    ms_gates2 = []
+    for dy in dys2:
+        ms_gates2.append(R(basis, phi, theta, dx, dy, omega, rabi_rate, target_spins, sparse, amp_mod))
+    fidelities = [gate.compute_process_fidelity(ideal_R(basis, phi, theta, target_spins).process_matrix) for gate in ms_gates]
+    fidelities2 = [gate.compute_process_fidelity(ideal_R(basis, phi, theta, target_spins).process_matrix) for gate in ms_gates2]
+    approx_fids = [
+        interpolated_R(dx, dy).compute_process_fidelity(
+            ideal_R(basis, phi, theta, target_spins).process_matrix
+        ) for dy in dys2
+    ]
 
-        dy_scale = 1/(2*np.pi*1e3)
-        plt.plot(dys * dy_scale, 1-np.array(fidelities), 'o', label='simulation: grid point')
-        plt.plot(dys2 * dy_scale, 1-np.array(approx_fids), '-', label='interpolation')
-        plt.plot(dys2 * dy_scale, 1-np.array(fidelities2), '.', label='simulation: off grid')
-        plt.xlabel(f'Half-Width of Boxed White Noise (kHz)')
-        plt.ylabel('Infidelity')
-        plt.legend()
-        plt.savefig(data_directory / f'infidelity_vs_{dy_name}.pdf', bbox_inches='tight')
-        plt.show()
+    dy_scale = 1/(2*np.pi*1e3)
+    plt.plot(dys * dy_scale, 1-np.array(fidelities), 'o', label='simulation: grid point')
+    plt.plot(dys2 * dy_scale, 1-np.array(approx_fids), '-', label='interpolation')
+    plt.plot(dys2 * dy_scale, 1-np.array(fidelities2), '.', label='simulation: off grid')
+    plt.xlabel(f'Half-Width of Boxed White Noise (kHz)')
+    plt.ylabel('Infidelity')
+    plt.legend()
+    plt.savefig(data_directory / f'infidelity_vs_{dy_name}.pdf', bbox_inches='tight')
+    plt.show()
 
 if __name__ == '__main__':
     main()
