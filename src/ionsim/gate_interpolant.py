1fe3892f7fbcd1b764bd1a8e1d2829f07f4de446
Ethan McGarrigle <ecmcgar@sandia.gov>
2026-08-28 17:05:38 -0600

    clean up one of internal function builders from Jay's comment

--- src/ionsim/gate_interpolant.py
+++ src/ionsim/gate_interpolant.py
@@ -217,32 +217,14 @@ class GateInterpolant():
 
         # Build and return a general, dynamic interpolating function 
         def _interpolating_function(*args, **kwargs):
-            # Interpolating function for the property specified in the interpolant.  
-            if args and kwargs:
-                raise IonSimError("Use positional or keyword arguments, not both.")
+            if len(args) == 1 and not kwargs and isinstance(args[0], (list, tuple, np.ndarray)):
+                   args = tuple(args[0])
+            
+            sig = inspect.Signature(parameters=[inspect.Parameter(name, inspect.Parameter.POSITIONAL_OR_KEYWORD)
+                for name in self.parameter_list] )
+            
+            grid_coordinate = sig.bind(*args, **kwargs).arguments
 
-            if args:
-                if len(args) == 1 and hasattr(args[0], '__len__'):
-                    # If one argument is passed (representing a list, tuple, array of arguments), convert to list
-                    values = list(args[0])
-                else:
-                    # If several arguments are passed, this should correspond to the parameter values. 
-                    values = list(args)
-
-                if len(values) != N_parameters:
-                    raise IonSimError(f"Not enough parameters specified for interpolant. Expected {N_parameters}, received {len(values)}.")
-
-                grid_coordinate = dict(zip(self.parameter_list, values))
-            else:
-                missing_parameters = set(self.parameter_list).difference(set(kwargs.keys()))
-                extra_parameters = set(kwargs.keys()).difference(set(self.parameter_list)) 
-                if missing_parameters:
-                    raise IonSimError(f"Function keyword arguments are missing the following parameters: {missing_parameters}")
-                if extra_parameters:
-                    raise IonSimError(f"Additional parameters specified that are not part of the interpolation parameter list: {extra_parameters}")
-
-                grid_coordinate = kwargs               
- 
             # Convert grid coordinate dictionary to tuple 
             grid_coordinate_values = tuple(grid_coordinate[parameter_name] for parameter_name in self.parameter_list) # ensures sorted order of grid coordinate values
 
